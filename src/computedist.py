5dff1fc484969c2e7841b3d7508a2e931d653b87
mbarakatt_abacus <mbarakat@abacus1.(none)>
2014-06-03 16:24:14 -0400

    more

--- src/computedist.py
+++ src/computedist.py
@@ -1,4 +1,8 @@
 from zipdist import *
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.misc import imread
+
 
 ZC=Zip_Codes("../data/zipcode/zipcode.csv")
 print "Done loading the zip codes"
@@ -7,8 +11,17 @@ print ZC.get_distance("07304","30006")
 
 
 findiv_pos=open("/lb/project/gravel/data/SCCS/crossref/ors_124_115.txt",'r')
+findiv_pos.readline()#removing the first line
 
-indiv_pos=[]
+indiv_posx=[]
+indiv_posy=[]
 for line in findiv_pos:
 	sp = line.split()
-	indiv_pos.append((float(sp[4]),float(sp[5])))
+	indiv_posx.append(float(sp[3]))
+	indiv_posy.append(float(sp[4]))
+
+
+img = imread("../data/usa-map.jpg")
+plt.scatter(indiv_posx,indiv_posy,zorder=1)
+plt.imshow(img,zorder=0, extent=[-95, -75,26, 40])
+plt.show()
